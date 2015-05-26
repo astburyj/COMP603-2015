4cfd3174f856cdf3a60fcdbae64479b84d4b9b38
James Astbury <astburyj@wit.edu>
2015-05-26 12:04:05 -0400

    changes

--- src/brainfuck.cpp
+++ src/brainfuck.cpp
@@ -1,8 +1,6 @@
 /*
 = Brainfuck
-
 If you have gcc:
-
 ----
 g++ -o brainfuck.exe brainfuck.cpp
 brainfuck.exe helloworld.bf
@@ -73,13 +71,18 @@ class CommandNode : public Node {
         }
 };
 
+class Container: public Node {
+    public:
+        vector<Node*> children;
+        virtual void accept (Visitor * v) = 0;
+};
+
 /**
  * Loop publicly extends Node to accept visitors.
  * Loop represents a loop in Brainfuck.
  */
-class Loop : public Node {
+class Loop : public Container {
     public:
-        vector<Node*> children;
         void accept (Visitor * v) {
             v->visit(this);
         }
@@ -89,9 +92,8 @@ class Loop : public Node {
  * Program is the root of a Brainfuck program abstract syntax tree.
  * Because Brainfuck is so primitive, the parse tree is the abstract syntax tree.
  */
-class Program : public Node {
+class Program : public Container {
     public:
-        vector<Node*> children;
         void accept (Visitor * v) {
             v->visit(this);
         }
@@ -99,20 +101,23 @@ class Program : public Node {
 
 /**
  * Read in the file by recursive descent.
- * Modify as necessary and add whatever functions you need to get things done.
+ * Modify as necessary and add whatever functions you need to get things done. 20 lines
  */
-void parse(fstream & file, Program * program) {
+void parse(fstream & file, Container * container) { // container is program node
+	// while loop read in characters one at a time
     char c;
     // How to peek at the next character
-    c = (char)file.peek();
-    // How to print out that character
-    cout << c;
-    // How to read a character from the file and advance to the next character
-    file >> c;
-    // How to print out that character
-    cout << c;
-    // How to insert a node into the program.
-    program->children.push_back(new CommandNode(c));
+	while ((char)file.peek() != -1) {
+		// How to read a character from the file and advance to the next character
+		file >> c;
+		// How to print out that character
+		//cout << c;
+		// How to insert a node into the container.
+		container->children.push_back(new CommandNode(c));
+	}
+	/* if loop recursive call - its a new level
+		[ call parse recursively
+		] return from parse*/
 }
 
 /**
@@ -143,6 +148,7 @@ class Printer : public Visitor {
             for (vector<Node*>::const_iterator it = program->children.begin(); it != program->children.end(); ++it) {
                 (*it)->accept(this);
             }
+            cout << '\n';
         }
 };
 
